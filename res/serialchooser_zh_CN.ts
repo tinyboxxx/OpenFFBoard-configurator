94cf68babe8fa7e1975dbf78a1296838a7cf2d12
tinyboxxx <hcxtxwd_ok@126.com>
2021-06-21 22:27:03 +0800

    尝试翻译，还没有在代码中实现

---
+++ res/serialchooser_zh_CN.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="serialchooser.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="serialchooser.ui" line="20"/>
+        <source>Send</source>
+        <translation type="unfinished">发送</translation>
+    </message>
+    <message>
+        <location filename="serialchooser.ui" line="27"/>
+        <source>manual command (help)</source>
+        <translation type="unfinished">手动输入命令（发送help获得帮助）</translation>
+    </message>
+    <message>
+        <location filename="serialchooser.ui" line="34"/>
+        <source>Refresh</source>
+        <translation type="unfinished">刷新</translation>
+    </message>
+    <message>
+        <location filename="serialchooser.ui" line="51"/>
+        <source>Connect</source>
+        <translation type="unfinished">连接设备</translation>
+    </message>
+</context>
+</TS>
---
+++ res/tmc4671_ui_zh_CN.ts
@@ -0,0 +1,152 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="tmc4671_ui.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="20"/>
+        <source>TMC4671</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="38"/>
+        <source>PIDs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="44"/>
+        <source>Torque P</source>
+        <translation type="unfinished">扭矩 P</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="58"/>
+        <source>Torque I</source>
+        <translation type="unfinished">扭矩 I</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="72"/>
+        <source>Flux P</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="86"/>
+        <source>Flux I</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="100"/>
+        <source>Change pids</source>
+        <translation type="unfinished">修改PID</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="107"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Toggles between parallel and sequential PI modes. Requires retuning.&lt;br/&gt;Advanced (sequential) can improve impulse response.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="110"/>
+        <source>Advanced mode</source>
+        <translation type="unfinished">高级模式</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="126"/>
+        <source>Motor</source>
+        <translation type="unfinished">电机参数</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="132"/>
+        <source>Motor Type</source>
+        <translation type="unfinished">电机类型</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="140"/>
+        <source>None</source>
+        <translation type="unfinished">无</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="145"/>
+        <source>DC</source>
+        <translation type="unfinished">DC 直流电机</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="150"/>
+        <source>Stepper</source>
+        <translation type="unfinished">步进电机</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="155"/>
+        <source>BLDC</source>
+        <translation type="unfinished">无刷直流电机</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="163"/>
+        <source>Pole Pairs</source>
+        <translation type="unfinished">极对数</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="177"/>
+        <source>Encoder Type</source>
+        <translation type="unfinished">编码器类型</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="185"/>
+        <source>NONE</source>
+        <translation type="unfinished">无编码器</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="193"/>
+        <source>CPR (4*PPR)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="203"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Encoder resolution after quadrature (4 * PPR = CPR)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="213"/>
+        <source>Change motor</source>
+        <translation type="unfinished">修改电机类型</translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="236"/>
+        <source>More</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="242"/>
+        <source>Max field weakening</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="256"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Can increase max speed.&lt;/p&gt;&lt;p&gt;Caution! Too high will cause&lt;br/&gt;overshoot or even runaway!&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="270"/>
+        <source>Align Encoder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="277"/>
+        <source>Motor current:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="284"/>
+        <source>0A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="tmc4671_ui.ui" line="310"/>
+        <source>%p% Power</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
